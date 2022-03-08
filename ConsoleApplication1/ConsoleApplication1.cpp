db96da920799cddc5201b92451f6ecfc3c01198d
Saif Asif <fluorine2005@gmail.com>
2022-03-08 17:21:24 +0000

    fixed it :)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,23 +2,23 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main(int argc, char**argv) {
 	std::cout << "M5t2 is the GOAT at this" << endl;
 	string sentence = "My name is Fluorine and i am exploring c++ \n";
 	std::cout << sentence;
-	cout << "The length of the text above is : " << sentence.length() << " characters long" << endl;
-	std::cout << "THIS INFORMATION WILL NOT BE STORED!" << endl;
+	cout << "The length of the text above is : " << sentence.length() << " characters long \n";
+	std::cout << "THIS INFORMATION WILL NOT BE STORED! \n";
 	string name;
-	std::cout << "Enter your name " << endl;
+	std::cout << "Enter your name \n";
 	cin >> name;
+	std::cout << "Your name is " << name  << endl;
 	int age;
-	std::cout << "What is your age ";
+	std::cout << "What is your age \n";
 	cin >> age;
+	std::cout << "Your Age is " << age << endl;
 	string country;
-	std::cout << "Enter what country you live in " << endl;
+	std::cout << "Enter what country you live in \n";
 	cin >> country;
-	std::cout << "Your name is " << name << endl;
-	std::cout << "Your age is " << age << endl;
 	std::cout << "You live in " << country << endl;
 	const float PI = 3.14;
 	std::cout << "the word PI in numbers is " << PI << endl;
@@ -28,5 +28,5 @@ int main() {
 	std::cout << "not random number " << sum1 << endl;
 	std::cout << "not random numbers " << sum2 << endl;
 	std::cout << "completly not random number's " << sum3 << endl;
-	return EXIT_SUCCESS;
+	system("pause");
 }
